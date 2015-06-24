8f1ab636e4d30b8c2c2c2adf0c4a286edadca938
Abdul Syed <abdul.baqi.syed@gmail.com>
2015-06-24 02:22:08 -0700

    changes

--- spotify_poetry.py
+++ spotify_poetry.py
@@ -5,12 +5,23 @@
 ## I have made two attempts to solve this via two methods:
 ##
 ## 1) Iterate over consecutive words or tokens.
+## DIDNT HAVE TIME TO IMPLEMENT NLP BELOW.
 ## 2) Use Natural Language Processing (NLP) library to extract parts of speech.
 ##
 ## For part 1 logic is described below:
 ## Main assumptions:
 ##  1) Song titles can't be more than 6 words.
-##  2) Song titles are not shorter than  
+##  2) Song titles are not shorter than 3 words.
+##  3) Strip out of all punctuation.
+##  4) song title must be a 100% match to the phrase.
+##
+##TODO:
+##  1) Add memoization to avoid same queries being sent to spotipy.
+##  2) Add parallelization, so multiple queries can be sent to spotipy, to speed up.
+##  3) Add ability to do % match. Currently song title has to match exactly the phrase.
+##     Add ability to calculate percent of words matching, to show greater results.
+##  4) Track phrases with no results. 
+##
 ## author: Abdul Syed (abdul.baqi.syed@gmail.com)
 
 import re
@@ -23,7 +34,10 @@ class SpotifyPoetry(object):
   """Class to store functions/objects which will input poetry from users and output spotify playlist."""
 
   def __init__(self):
-    """Initialize internal set which will hold all tokens."""
+    """Initialize internal set which will hold all tokens.
+
+    _tokens: set which will contain unique tokens(str) from a given phrase.
+    """
     self._tokens = set()
 
   def _ReturnSpotifyService(self):
@@ -31,15 +45,35 @@ class SpotifyPoetry(object):
     return spotipy.Spotify()
 
   def _ProcessPhrase(self, phrase):
-    """Removes any spaces or characters using RegExp and returns a list of words in the phrase."""
-    return re.split(r"[\s\+*.,!\?]+", phrase)
+    """Helper function to removes any spaces or characters using RegExp.
+
+    Args:
+      phrase: str, a user inputted phrase to search.
+
+    Returns:
+      list of words in the phrase, removing any special characters.
+    """
+    return re.split(r"[\s\+*.,!\?]+", phrase.lower().replace("'",""))
 
   def _ReturnSongByTitle(self, phrase):
-    """Returns results for songs."""
+    """Returns results for songs.
+
+    Args:
+      phrase: str, a user inputted phrase to search.
+
+    Returns:
+      Response from Spotify service based on search token and type track.
+    """
     return self._ReturnSpotifyService().search(q=phrase, type="track")
  
   def _TokenizePhrase(self, phrase, min_words_in_song=3, max_words_in_song=6):
-    """Sets the internal set with tokenized phrases."""
+    """Sets the internal set with tokenized phrases.
+
+    Args:
+      phrase: str, a user inputted search phrase.
+      min_words_in_song: int, minimum words in a song title. Default to 3.
+      max_words_in_song: int, maximum words in a song title. Default to 6
+    """
     total_words = len(phrase)
     # Go backwards
     total_range = range(min(total_words, max_words_in_song), min_words_in_song - 1, -1)
@@ -51,46 +85,42 @@ class SpotifyPoetry(object):
 
   #TODO: Need to add better exception handling. Right now, it's just continuing.
   def _GetSongs(self, tokens):
-    """Function which calls spotify service and returns songs."""
-    song_titles = {}
+    """Function which calls spotify service and returns songs. Songs are ordered by popularity field in results.
+    Args:
+      tokens: list of words.
+
+    Returns:
+      song_titles: list, containing unique songs and artists. 
+    """
+    song_titles = set()
     for row in tokens:
       try:
-        result = self._ReturnSongByTitle(row.lower().strip())
+        search_token = row.lower().strip()
+        result = self._ReturnSongByTitle(search_token)
         if result:
-          length = len(result['tracks']['items'])
-          for num in xrange(length):
+          len_of_results = len(result['tracks']['items'])
+          for num in xrange(len_of_results):
             artist = str(result['tracks']['items'][num]['artists'][0]['name']).lower().strip()
-            title = str(result['tracks']['items'][num]['name']).lower()
-            popularity = result['tracks']['items'][0]['popularity']
-            if title == row.lower().strip():
-              key = title + " by " + artist
-              if popularity not in song_titles:
-                song_titles[popularity] = [key]
-              else:
-                song_titles[popularity].append(key)
-            else:
-              print title +" by" + artist + " != " + row
+            title = self._ProcessPhrase(str(result['tracks']['items'][num]['name']).lower())
+            title = " ".join(title)
+            #TODO (implement popularity rank)
+            #popularity = result['tracks']['items'][0]['popularity']
+            key = title + " by " + artist
+            if search_token == title:
+              song_titles.add(key)
       except:
         continue
-    return song_titles
-
+    print "PLAYLIST FOR YOU <3<3<3"
+    for entry in song_titles:
+      print entry
 
   def GetPlaylist(self, phrase):
     """Input a phrase and output a playlist based on keywords."""
-    phrase = self._ProcessPhrase("If I can't let it go out of my mind")
+    phrase = self._ProcessPhrase(phrase)
     self._TokenizePhrase(phrase)
     playlist = self._GetSongs(self._tokens)
-    sorted_playlist_by_popularity = sorted(playlist.items())
-    sorted_set = set()
-    for row in list(reversed(sorted_playlist_by_popularity)):
-      for entry in row[1]:
-        sorted_set.add(entry)
-    for row in sorted_set:
-      print row
 
 if __name__ == "__main__":
   phrase = input("Enter favorite poetry with quotes: ")
-  phrase = str(phrase).lower()
   t = SpotifyPoetry()
   t.GetPlaylist(phrase)
-
